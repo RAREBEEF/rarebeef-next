6f59de88a6f1b83f6d8d6950a51b459c0575b23e
RAREBEEF <drrobot409@gmail.com>
2022-10-07 10:57:35 +0900

    Update contents of the diary

--- sections/diary.ts
+++ sections/diary.ts
@@ -2,7 +2,7 @@ import { sectionDataType } from "../types";
 import img1 from "../public/screenshots/diary-calendar.png";
 import img2 from "../public/screenshots/diary-diary.png";
 import img3 from "../public/screenshots/diary-write.png";
-import img4 from "../public/screenshots/diary-period.png";
+import img4 from "../public/screenshots/diary-tags.png";
 import img5 from "../public/screenshots/diary-responsive.png";
 
 const data: sectionDataType = {
@@ -18,7 +18,7 @@ const data: sectionDataType = {
     headCount: 1,
   },
   description:
-    "하루를 기록하는 일기장 웹 애플리케이션입니다.\n설치 가능한 PWA로 개발하였으며 Next.js를 활용한 첫 프로젝트입니다.\n처음 생각과는 다르게 달력 알고리즘을 작성한 부분이 가장 재밌었던 프로젝트입니다. 공휴일 API도 활용하여 달력에서 공휴일을 구분할 수 있도록 하였습니다.\n또한 영화 API와 음악 API를 활용해 그 날의 영화와 음악을 검색하여 추가할 수 있도록 하였습니다.",
+    "하루를 기록하는 일기장 웹 애플리케이션입니다.\n설치 가능한 PWA로 개발하였으며 Next.js를 활용한 첫 프로젝트입니다.\n처음 생각과는 다르게 달력 알고리즘을 작성한 부분이 가장 재밌었던 프로젝트입니다.\n공휴일, 영화, 음악 API를 활용해 다양한 기능을 추가하였습니다.\n또한 태그 기능을 구현하여 태그에 해당하는 일기를 모아볼 수 있도록 하였습니다.",
   skills: [
     "HTML",
     "TypeScript",
