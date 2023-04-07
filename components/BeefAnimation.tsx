d69c40815a3fb911173ace58006eb689363a5840
RAREBEEF <drrobot409@gmail.com>
2023-04-07 10:38:21 +0900

    인트로 로딩 업데이트

--- components/BeefAnimation.tsx
+++ components/BeefAnimation.tsx
@@ -1,106 +1,168 @@
 import classNames from "classnames";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./BeefAnimation.module.scss";
 import NextImage from "next/image";
 import arrowIcon from "../public/icons/angle-left-solid.svg";
 import Link from "next/link";
 import Cube from "./Cube";
+import altImage from "../public/animation/beef_animation_0120.jpg";
+import Button from "./Button";
 
 const BeefAnimation = () => {
   const [init, setInit] = useState<boolean>(false);
   const [cubeUnmaount, setCubeUnmount] = useState<boolean>(false);
+  const [canISkip, setCanISkip] = useState<boolean>(false);
+  const [skipAnimation, setSkipAnimation] = useState<boolean>(false);
   const [showBtn, setShowBtn] = useState<boolean>(false);
   const [showScrollGuide, setShowScrollGuide] = useState<boolean>(true);
-  const [imgElements, setImgElements] = useState<Array<JSX.Element>>([]);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+  const MAX_FRAME = 120;
+  const curFrame = useCallback(
+    (index: number) =>
+      `/animation/beef_animation_${index.toString().padStart(4, "0")}.jpg`,
+    []
+  );
 
+  // 애니메이션 스킵
   useEffect(() => {
-    if (!containerRef.current || !canvasRef.current) return;
+    if (skipAnimation) {
+      setSkipAnimation(true);
+      setInit(true);
+      setShowScrollGuide(false);
+      setShowBtn(true);
+      setTimeout(() => setCubeUnmount(true), 700);
+    }
+  }, [skipAnimation]);
+
+  // 모든 이미지가 준비되었는지 체크
+  useEffect(() => {
+    const allImgs: Array<HTMLImageElement> = [];
+    let waitingForLoad: Array<HTMLImageElement> = [];
+    let isAllReady = false;
+    let isQueueEnd = false;
+    let isTimerEnd = false;
+
+    const executeWhenReady = () => {
+      console.log("All frames are ready.");
+      clearTimeout(youCanSkip);
+      setInit(true);
+      setTimeout(() => setCubeUnmount(true), 700);
+    };
+
+    // 최소 로딩 시간
+    setTimeout(() => {
+      isTimerEnd = true;
+      isAllReady && executeWhenReady();
+    }, 2000);
+
+    // 스킵 버튼 출력하기
+    const youCanSkip = setTimeout(() => {
+      setCanISkip(true);
+    }, 10000);
+
+    // 이미지 불러오기
+    for (let i = 0; i <= MAX_FRAME; i++) {
+      const img = new Image();
+      img.src = curFrame(i);
+      allImgs.push(img);
+    }
+
+    /**
+     * 모든 이미지의 로드 여부를 1회씩 체크
+     * 아직 로드되지 않은 이미지는 waitingForLoad에 등록
+     * 큐가 완료되었을 때 로드되지 않은 이미지가 없다면 모든 로딩 프로세스 종료
+     * */
+    const imgLoadCheckQueue = (imgs: Array<HTMLImageElement>) => {
+      // 모든 큐 완료
+      if (!imgs || imgs.length === 0) {
+        console.log("Queue completed. Waiting for all frames to load.");
+        isQueueEnd = true;
+
+        // 모든 이미지 로드 완료 여부
+        if (!waitingForLoad || waitingForLoad.length === 0) {
+          isTimerEnd && executeWhenReady();
+          isAllReady = true;
+        }
+
+        return;
+      }
+
+      const current = imgs[0];
+
+      // 로드 완료/미완료 이미지 구분
+      if (current.complete) {
+        console.log(current.src.slice(-8) + " is ready.");
+      } else {
+        waitingForLoad.push(current);
+
+        current.onload = () => {
+          console.log(current.src.slice(-8) + " is ready.");
+          checkNotLoadedImgs(waitingForLoad);
+        };
+      }
+
+      // 다음 큐
+      imgs.shift();
+      imgLoadCheckQueue(imgs);
+    };
+
+    imgLoadCheckQueue(allImgs);
+
+    /**
+     * 로드가 왼료되지 않았던 이미지들의 완료 여부 체크
+     * */
+    const checkNotLoadedImgs = (imgs: Array<HTMLImageElement>) => {
+      if (!imgs || imgs.length === 0 || isAllReady) {
+        return;
+      }
+
+      const notLoaded = waitingForLoad.filter((img) => !img.complete);
+
+      if (notLoaded.length === 0 && isQueueEnd) {
+        isTimerEnd && executeWhenReady();
+        isAllReady = true;
+
+        return;
+      }
+
+      waitingForLoad = notLoaded;
+    };
+  }, [curFrame]);
+
+  // 스크롤에 맞춰 캔버스에 이미지 렌더링
+  useEffect(() => {
+    if (!containerRef.current || !canvasRef.current || !init || skipAnimation)
+      return;
+
     const cvs = canvasRef.current;
     const ctx = cvs.getContext("2d");
     if (!ctx) return;
     const container = containerRef.current;
-    const maxFrame = 120;
-    const curFrame = (index: number) =>
-      `/animation/beef_animation_${index.toString().padStart(4, "0")}.jpg`;
     cvs.width = 768;
     cvs.height = 672;
 
-    const imgs: Array<JSX.Element> = [];
-
-    // const preload = () => {
-    //   // let isTimerEnd = false;
-    //   // let isLoadEnd = false;
-
-    //   // const executeWhenReady = () => {
-    //   //   setInit(true);
-    //   //   setTimeout(() => setCubeUnmount(true), 700);
-    //   // };
-
-    // //   const relayLoadCheck = (i: number) => {
-    // //     if (i > maxFrame) {
-    // //       console.log("all frames are ready");
-    // //       isLoadEnd = true;
-    // //       isTimerEnd && executeWhenReady();
-    // //       return;
-    // //     }
-
-    // //     const img = new Image();
-    // //     img.src = imgs[i].props.src;
-
-    // //     if (img.complete) {
-    // //       console.log(`frame ${i} ready`);
-    // //       relayLoadCheck(i + 1);
-    // //     } else {
-    // //       img.onload = () => {
-    // //         console.log(`frame ${i} ready`);
-    // //         relayLoadCheck(i + 1);
-    // //       };
-    // //     }
-    // //   };
-
-    // //   relayLoadCheck(0);
-    // // };
-    // }
-
-    setTimeout(() => {
-      setInit(true);
-      setTimeout(() => setCubeUnmount(true), 700);
-    }, 2000);
-
-    for (let i = 0; i <= maxFrame; i++) {
-      const nextImg = <img key={i} src={curFrame(i)} alt="RAREBEEF" />;
-      imgs.push(nextImg);
-    }
-
-    setImgElements(imgs);
-
-    // preload();
-
     const img = new Image();
-
-    img.src = imgs[0].props.src;
+    img.src = curFrame(0);
 
     img.onload = function () {
       ctx.drawImage(img, 0, 0);
     };
 
     const update = (i: number) => {
-      img.src = imgs[i].props.src;
+      img.src = curFrame(i);
       ctx.drawImage(img, 0, 0);
     };
 
-    const renderCurrentImage = (e?: Event) => {
-      const scrollProgress = Math.abs(
-        container.getBoundingClientRect().top /
-          (container.clientHeight - window.innerHeight)
-      );
+    const windowScrollHandler = () => {
+      const scrollProgress =
+        -container.getBoundingClientRect().top /
+        (container.clientHeight - window.innerHeight);
 
       if (scrollProgress > 1) return;
 
-      const frame = Math.ceil(scrollProgress * maxFrame) || 0;
+      const frame = Math.ceil(scrollProgress * MAX_FRAME);
 
       requestAnimationFrame(() => update(frame));
 
@@ -110,35 +172,59 @@ const BeefAnimation = () => {
         setShowScrollGuide(true);
       }
 
-      if (!(e instanceof Event) || scrollProgress < 0.8) {
+      if (scrollProgress < 0.8) {
         setShowBtn(false);
       } else {
         setShowBtn(true);
       }
     };
 
-    renderCurrentImage();
-
-    window.addEventListener("scroll", (e) => renderCurrentImage(e));
+    window.addEventListener("scroll", windowScrollHandler);
 
     return () => {
-      window.removeEventListener("scroll", (e) => renderCurrentImage(e));
+      window.removeEventListener("scroll", windowScrollHandler);
     };
-  }, []);
+  }, [curFrame, init, skipAnimation]);
 
   return (
     <section
-      className={classNames(styles.container, init && styles.init)}
+      className={classNames(
+        styles.container,
+        init && styles.init,
+        skipAnimation && styles["skiped"]
+      )}
       ref={containerRef}
     >
       {!cubeUnmaount && (
-        <div className={classNames(styles.loading, init && styles.hidden)}>
+        <div
+          className={classNames(
+            styles.loading,
+            init && styles.hidden,
+            canISkip && styles["show-skip"]
+          )}
+        >
           <Cube />
+          <p>Loading</p>
+          <Button
+            onClick={() => setSkipAnimation(true)}
+            text="인트로 스킵하기"
+          />
         </div>
       )}
-      <div ref={wrapperRef} className={styles.wrapper}>
-        <canvas className={styles.canvas} ref={canvasRef} />
-      </div>
+      {skipAnimation ? (
+        <div className={styles["alt-image-wrapper"]}>
+          <NextImage
+            src={altImage}
+            alt="RAREBEEF's portfolio"
+            fill={true}
+            objectFit="contain"
+          />
+        </div>
+      ) : (
+        <div ref={wrapperRef} className={styles.wrapper}>
+          <canvas className={styles.canvas} ref={canvasRef} />
+        </div>
+      )}
 
       <nav
         className={classNames(
@@ -165,177 +251,8 @@ const BeefAnimation = () => {
           alt="Scroll down"
         />
       </div>
-      <div className={styles.preload}>{imgElements}</div>
     </section>
   );
 };
 
 export default BeefAnimation;
-
-// import classNames from "classnames";
-// import { useEffect, useRef, useState } from "react";
-// import styles from "./BeefAnimation.module.scss";
-// import NextImage from "next/image";
-// import arrowIcon from "../public/icons/angle-left-solid.svg";
-// import Link from "next/link";
-// import Cube from "./Cube";
-// import Image from "next/image";
-
-// const BeefAnimation = () => {
-//   const [init, setInit] = useState<boolean>(false);
-//   const [cubeUnmaount, setCubeUnmount] = useState<boolean>(false);
-//   const [showBtn, setShowBtn] = useState<boolean>(false);
-//   const [showScrollGuide, setShowScrollGuide] = useState<boolean>(true);
-//   const [imgs, setImgs] = useState<Array<JSX.Element> | null>(null);
-//   const canvasRef = useRef<HTMLCanvasElement>(null);
-//   const wrapperRef = useRef<HTMLDivElement>(null);
-//   const containerRef = useRef<HTMLDivElement>(null);
-//   const maxFrame = 120;
-
-//   // 이미지 로딩 체크
-//   useEffect(() => {
-//     if (!imgs) return;
-
-//     const executeWhenReady = () => {
-//       setInit(true);
-//       setTimeout(() => {
-//         setCubeUnmount(true);
-//       }, 700);
-//     };
-
-//     // 이미 로드된 경우
-//     if (imgs[120].complete) {
-//       executeWhenReady();
-//       // 최초 로드일 경우
-//     } else {
-//       let isLoadEnd = false;
-//       let isTimerEnd = false;
-
-//       // 최소 로딩 시간(2초)
-//       setTimeout(() => {
-//         isTimerEnd = true;
-//         isLoadEnd && executeWhenReady();
-//       }, 2000);
-
-//       // 이미지 로드 체크
-//       imgs[120].onload = () => {
-//         isLoadEnd = true;
-//         isTimerEnd && executeWhenReady();
-//       };
-//     }
-//   }, [imgs]);
-
-//   // 이미지 로드
-//   useEffect(() => {
-//     const curFrame = (index: number) =>
-//       `/animation/beef_animation_${index.toString().padStart(4, "0")}.jpg`;
-
-//     const preloadImgs: Array<JSX.Element> = [];
-
-//     for (let i = 0; i <= maxFrame; i++) {
-//       // const img = new Image();
-//       // img.src = curFrame(i);
-//       const img = <Image src={curFrame(i)} alt={`RAREBEEF animation frame ${i}`} />
-//       preloadImgs.push(img);
-//     }
-
-//     setImgs(preloadImgs);
-//   }, []);
-
-//   // 렌더
-//   useEffect(() => {
-//     if (!containerRef.current || !canvasRef.current || !imgs) return;
-
-//     const cvs = canvasRef.current;
-//     const ctx = cvs.getContext("2d");
-
-//     if (!ctx) return;
-
-//     const container = containerRef.current;
-
-//     cvs.width = 768;
-//     cvs.height = 672;
-
-//     // 사진 교체
-//     const update = (i: number) => {
-//       const img = imgs[i];
-//       ctx.drawImage(img, 0, 0);
-//     };
-
-//     const renderCurImg = (e?: Event) => {
-//       const scrollProgress = Math.abs(
-//         container.getBoundingClientRect().top /
-//           (container.clientHeight - window.innerHeight)
-//       );
-
-//       const frame = Math.ceil(scrollProgress * maxFrame) || 0;
-
-//       requestAnimationFrame(() => update(frame));
-
-//       if (scrollProgress > 0.1) {
-//         setShowScrollGuide(false);
-//       } else {
-//         setShowScrollGuide(true);
-//       }
-
-//       if (!(e instanceof Event) || scrollProgress < 0.8) {
-//         setShowBtn(false);
-//       } else {
-//         setShowBtn(true);
-//       }
-//     };
-
-//     renderCurImg();
-
-//     window.addEventListener("scroll", (e) => renderCurImg(e));
-
-//     return () => {
-//       window.removeEventListener("scroll", (e) => renderCurImg(e));
-//     };
-//   }, [imgs]);
-
-//   return (
-//     <section
-//       className={classNames(styles.container, init && styles.init)}
-//       ref={containerRef}
-//     >
-//       {!cubeUnmaount && (
-//         <div className={classNames(styles.loading, init && styles.hidden)}>
-//           <Cube />
-//         </div>
-//       )}
-//       <div ref={wrapperRef} className={styles.wrapper}>
-//         <canvas className={styles.canvas} ref={canvasRef} />
-//       </div>
-
-//       <nav
-//         className={classNames(
-//           styles["to-projects-wrapper"],
-//           showBtn && styles.show
-//         )}
-//       >
-//         <Link href="/projects" className={styles["to-projects"]}>
-//           Link to project list
-//         </Link>
-//       </nav>
-
-//       <div
-//         className={classNames(
-//           styles["scroll-guide"],
-//           showScrollGuide && styles.show
-//         )}
-//       >
-//         <NextImage
-//           src={arrowIcon}
-//           width={50}
-//           height={50}
-//           priority
-//           alt="Scroll down"
-//         />
-//       </div>
-//       <div>{imgs?.map((img) =>)}</div>
-//     </section>
-//   );
-// };
-
-// export default BeefAnimation;
